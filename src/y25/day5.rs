e6efd8e4fcbaabfcbab04c1261f59e3b57316bce
alexjneves <3845319+alexjneves@users.noreply.github.com>
2025-12-05 20:33:45 -0800

    2025 day 5 tidy

--- src/y25/day5.rs
+++ src/y25/day5.rs
@@ -7,17 +7,21 @@ const DAY_ID: u8 = 5;
 
 pub struct Day5 {}
 
+#[derive(Debug)]
+struct IngredientDatabase {
+    fresh_ingredient_ranges: Vec<Range<u64>>,
+    available_ingredients: Vec<u64>
+}
+
 impl Day for Day5 {
     fn run(&self, part: Part, input_type: InputType) -> i64 {
         let input = read_day_input(YEAR_ID, DAY_ID, &part, &input_type);
 
         let ingredient_database = parse_ingredient_database(&input);
 
-        println!("{:?}", ingredient_database);
-
         match part {
             Part::One => part1(&ingredient_database),
-            Part::Two => part2_efficient(&ingredient_database)
+            Part::Two => part2(&ingredient_database)
         }
     }
 }
@@ -37,26 +41,13 @@ fn part1(ingredient_database: &IngredientDatabase) -> i64 {
     fresh_ingredient_count
 }
 
-// Brute force
 fn part2(ingredient_database: &IngredientDatabase) -> i64 {
-    let mut fresh_ingredient_ids: HashSet<u64> = HashSet::new();
-
-    for range in &ingredient_database.fresh_ingredient_ranges {
-        for id in range.start..=range.end {
-            fresh_ingredient_ids.insert(id);
-        }
-    }
-
-    fresh_ingredient_ids.len() as i64
-}
-
-fn part2_efficient(ingredient_database: &IngredientDatabase) -> i64 {
     let mut reduced_ranges = ingredient_database.fresh_ingredient_ranges.clone();
 
     loop {
         let (updated_ranges, reduced) = reduce_ranges(&reduced_ranges);
 
-        // once no reductions took place we've collapsed all of the overlapping ranges
+        // when no reductions occurred we've collapsed all of the overlapping ranges
         if !reduced {
             break;
         }
@@ -70,36 +61,17 @@ fn part2_efficient(ingredient_database: &IngredientDatabase) -> i64 {
         fresh_ingredients_count += (range.end - range.start + 1) as i64;
     }
 
-    reduced_ranges.sort_by(|r1, r2| r1.start.cmp(&r2.start));
-    //println!("{:?}", reduced_ranges);
-
-    // for i in 0..reduced_ranges.len() - 2 {
-    //     let x = &reduced_ranges[i];
-    //     let y = &reduced_ranges[i + 1];
-
-    //     assert!(x.start <= x.end, "x.start <= x.end :: {} <= {}", x.start, x.end);
-    //     assert!(x.end < y.start, "x.end < y.start :: {} < {}", x.end, y.start);
-    //     assert!(y.start <= y.end, "y.start <= y.end :: {} <= {}", y.start, y.end);
-    // }
-
     fresh_ingredients_count
 }
 
 fn reduce_ranges(ranges: &Vec<Range<u64>>) -> (Vec<Range<u64>>, bool) {
     let mut reduced_ranges: Vec<Range<u64>> = vec![];
-
     let mut reduced = false;
 
     for range in ranges {
         let mut optimized_range_updated = false;
 
         for reduced_range in &mut reduced_ranges {
-            // DEBUGGING
-            if reduced_range.start == 121945158964045 || reduced_range.end == 121945158964045 {
-                println!("FOUND")
-            }
-            // END DEBUGGING
-
             // case: range fits within existing range
             if range.start >= reduced_range.start && range.end <= reduced_range.end {
                 optimized_range_updated = true;
@@ -116,36 +88,20 @@ fn reduce_ranges(ranges: &Vec<Range<u64>>) -> (Vec<Range<u64>>, bool) {
 
             // case: range overlaps with existing range max
             if (range.start >= reduced_range.start && range.start <= reduced_range.end) && range.end > reduced_range.end {
-                // 3      5
-                //           10      14
-
-                // 0      10
-                //        10     15
                 reduced_range.end = range.end;
                 optimized_range_updated = true;
-                if reduced_range.start == reduced_range.end {
-                    println!("ERROR: {}", reduced_range.start);
-                }
                 break;
             }
 
             // case: range overlaps with existing range min
             if (range.end >= reduced_range.start && range.end <= reduced_range.end) && range.start < reduced_range.start {
-                // 0      10
-                //     5        15
-
-                // 0      10
-                //        10     15
                 reduced_range.start = range.start;
-                optimized_range_updated = true; // dupe: 121945158964045..121945158964045
-                if reduced_range.start == reduced_range.end {
-                    println!("ERROR: {}", reduced_range.start);
-                }
+                optimized_range_updated = true;
                 break;
             }
         }
 
-        // Push if it is a unique range
+        // push if it is a unique range
         if !optimized_range_updated {
             reduced_ranges.push(range.clone());
         } else  {
@@ -154,12 +110,6 @@ fn reduce_ranges(ranges: &Vec<Range<u64>>) -> (Vec<Range<u64>>, bool) {
     }
 
     (reduced_ranges, reduced)
-}
-
-#[derive(Debug)]
-struct IngredientDatabase {
-    fresh_ingredient_ranges: Vec<Range<u64>>,
-    available_ingredients: Vec<u64>
 }
 
 fn parse_ingredient_database(input: &Vec<String>) -> IngredientDatabase {
@@ -186,6 +136,19 @@ fn parse_ingredient_database(input: &Vec<String>) -> IngredientDatabase {
     }
 
     IngredientDatabase { fresh_ingredient_ranges, available_ingredients }
+}
+
+#[allow(dead_code)]
+fn part2_brute_force(ingredient_database: &IngredientDatabase) -> i64 {
+    let mut fresh_ingredient_ids: HashSet<u64> = HashSet::new();
+
+    for range in &ingredient_database.fresh_ingredient_ranges {
+        for id in range.start..=range.end {
+            fresh_ingredient_ids.insert(id);
+        }
+    }
+
+    fresh_ingredient_ids.len() as i64
 }
 
 #[cfg(test)]
@@ -229,7 +192,7 @@ mod tests {
         let day5 = Day5 {};
         let answer = day5.run(Part::Two,InputType::Custom);
         
-        assert_eq!(answer, EXPECTED_ANSWER); // x.end < y.start :: 491517821437404 < 485988698650749
+        assert_eq!(answer, EXPECTED_ANSWER);
     }
 
     #[test]
